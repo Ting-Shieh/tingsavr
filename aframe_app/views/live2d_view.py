a81a18166c9e1d1a64cabe6768f707d36ade798f
Ting-Shieh <zsting29@gmail.com>
2021-06-23 18:53:02 +0800

    add live 2d test v1

---
+++ aframe_app/views/live2d_view.py
@@ -0,0 +1,7 @@
+from flask import render_template, request
+
+
+
+class ProjectTestViews():
+    def proejctTeste01():
+        return render_template("live2d_project/index.html")
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from aframe_app import app
 from aframe_app.views.aframe_views import AFrameViews
 from aframe_app.views.blog_views import BlogViews
-
+from aframe_app.views.live2d_view import ProjectTestViews
 
 
 @app.route('/', methods=['GET'])
@@ -21,35 +21,10 @@ def aframe01():
 def aframe02():
     return AFrameViews.aframe02()
 
+@app.route('/test1', methods=['GET'])
+def test1():
+    return ProjectTestViews.proejctTeste01()
 
-
-# @app.route('/')
-# def index():
-#     htmlpage = """
-#             <!DOCTYPE html>
-#             <html lang="en">
-#             <head>
-#                 <meta charset="UTF-8">
-#                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#                 <title>Ting's AR-01</title>
-#                 <script src="https://aframe.io/releases/0.9.0/aframe.min.js"></script>
-#                 <script src="https://rawgit.com/jeromeetienne/ar.js/master/aframe/build/aframe-ar.js"></script>
-#                 <script>THREEx.ArToolkitContext.baseURL = 'https://rawgit.com/jeromeetienne/ar.js/master/three.js/'</script>
-#             </head>
-#             <body style='margin : 0px; overflow: hidden;'>
-#
-#                 <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
-#                     <a-marker type='pattern' url='./res/maker.patt'>
-#                         <a-entity position='-3 2 0' text="width: 5; value:This is test"></a-entity>
-#                         <a-entity position='0 0 0' gltf-model='url(res/scene.gltf)'></a-entity>
-#                     </a-marker>
-#                     <a-entity camera></a-entity>
-#                 </a-scene>
-#             </body>
-#             </html>
-#     """
-#
-#     return htmlpage
 
 
 if __name__ == '__main__':
